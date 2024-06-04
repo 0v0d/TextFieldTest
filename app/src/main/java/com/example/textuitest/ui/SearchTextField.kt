017ca9c774b98df93fd6f864aa60ffdbb8e64ccb
0v0d <energyremtu@gmail.com>
2024-06-04 21:04:03 +0900

    update

--- app/src/main/java/com/example/textuitest/ui/SearchTextField.kt
+++ app/src/main/java/com/example/textuitest/ui/SearchTextField.kt
@@ -1,7 +1,6 @@
 package com.example.textuitest.ui
 
 import androidx.compose.foundation.border
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
